b656811b059ac4cf9938aa082ea985514398a437
geno <geno.racklinasher@gmail.com>
2017-09-26 15:07:17 +0100

    Travis & Testing

---
+++ testing/test_estimate.py
@@ -0,0 +1,15 @@
+import mock_prerelease
+
+
+def test_estimate():
+    estimate_checks = {
+        1: (580, 580.0),
+        7: (760, 108.57),
+        10: (850, 85.0),
+        11: (850, 77.27),
+        27: (1300, 48.15),
+        45: (1780, 39.56)
+    }
+
+    for students, estimates in estimate_checks.items():
+        assert mock_prerelease.estimate(students) == estimates
